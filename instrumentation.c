1ef2cbde8716c930c8d4563e65e97db5c5f67a58
Alaa Saab <alaa.saab@etu.univ-grenoble-alpes.fr>
2025-11-30 19:02:39 +0100

    completely removed recursivity and only left the bas version which works 90% of the time, if someone suggests any fixes please let me know, you can find the error by running: ./essai_fap < test.txt

--- instrumentation.c
+++ instrumentation.c
@@ -1,49 +1,21 @@
 #include <stdio.h>
 #include "fap.h"
 
-// CRITICAL: Do NOT include instrumentation.h here!
-// This file must call the REAL functions from fap_bug.c, not the instrumented wrappers
+int fap_compteur=0;
+int fap_taille_maximale=0;
 
-// Explicitly undefine any macros that might interfere (in case they're defined elsewhere)
-#ifdef inserer
-#undef inserer
-#endif
-#ifdef extraire
-#undef extraire
-#endif
-
-// Forward declare the real functions from fap_bug.c
-// Since this file doesn't include instrumentation.h, these will resolve to the real functions
-fap inserer(fap f, int element, int priorite);
-fap extraire(fap f, int *element, int *priorite);
-
-int fap_taille_courante = 0;
-int fap_taille_max = 0;
-
-fap instrumentation_inserer(fap f, int element, int priorite) {
-    // Direct call to real inserer - macros are NOT active in this file
-    f = inserer(f, element, priorite);
-
-    fap_taille_courante++;
-    if (fap_taille_courante > fap_taille_max) {
-        fap_taille_max = fap_taille_courante;
-        printf("[Instrumentation] Nouvelle taille max: %d\n", fap_taille_max);
+fap instrumentation_inserer(fap f, int element, int priorite)
+{
+    fap_compteur++;
+    if (fap_compteur > fap_taille_maximale) {
+        fap_taille_maximale = fap_compteur;
+        printf("TAILLE MAX FAP : %d\n", fap_compteur);
     }
-    return f;
-}
+    return inserer(f, element, priorite);
+};
 
-fap instrumentation_extraire(fap f, int *element, int *priorite) {
-    // Only decrement if fap is not empty
-    int was_empty = (f == NULL);
-    fap old = f;
-    
-    // Direct call to real extraire - macros are NOT active in this file
-    f = extraire(f, element, priorite);
-
-    // Decrement counter only if extraction actually happened
-    // (old was not NULL means fap had elements, and f != old means extraction occurred)
-    if (!was_empty && old != NULL && f != old) {
-        fap_taille_courante--;
-    }
-    return f;
+fap instrumentation_extraire(fap f, int *element, int *priorite)
+{
+    fap_compteur--;
+    return extraire(f, element, priorite);
 }
--- instrumentation.h
+++ instrumentation.h
@@ -1,18 +1,5 @@
-#ifndef INSTRUMENTATION_H
-#define INSTRUMENTATION_H
+extern int fap_compteur;
+extern int fap_taille_maximale;
 
-#include "fap.h"   
-
-// global counters
-extern int fap_taille_courante;
-extern int fap_taille_max;
-
-// prototypes for our wrapped functions
-fap instrumentation_inserer(fap f, int element, int priorite);
-fap instrumentation_extraire(fap f, int *element, int *priorite);
-
-// Replace calls to inserer/extraire with our wrappers
-#define inserer  instrumentation_inserer
+#define inserer instrumentation_inserer
 #define extraire instrumentation_extraire
-
-#endif
