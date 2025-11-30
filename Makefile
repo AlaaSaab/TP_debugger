e41a116af7b12f607e800b5bda8364d55dfdd153
Alaa Saab <alaa.saab@etu.univ-grenoble-alpes.fr>
2025-11-30 18:36:34 +0100

    instrumentation have a recursvie call problem, yet to solve, but the functions themselves are correct

--- Makefile
+++ Makefile
@@ -2,29 +2,29 @@ CC=clang -g
 
 all: essai_fap essai cesar
 
-%.o: %.c	
-	$(CC) -c $<
+essai_fap.o: essai_fap.c
+	$(CC) -include instrumentation.h -c essai_fap.c
 
 fap_bug.o: fap_bug.c fap.h
-
-essai_fap.o: essai_fap.c fap_bug.c fap.h
-	 $(CC) $(CFLAGS) -include instrumentation.h -c essai_fap.c
-
-essai.o: essai.c
-
-cesar.o: cesar.c
+	$(CC) -c fap_bug.c
 
 instrumentation.o: instrumentation.c
+	$(CC) -c instrumentation.c
 
-essai_fap: fap_bug.o essai_fap.o instrumentation.o
+essai_fap: essai_fap.o fap_bug.o instrumentation.o
 	$(CC) $^ -o $@
 
+essai.o: essai.c
+	$(CC) -c essai.c
+
 essai: essai.o
+	$(CC) $^ -o $@
+
+cesar.o: cesar.c
+	$(CC) -c cesar.c
 
 cesar: cesar.o
+	$(CC) $^ -o $@
 
 clean:
-	rm -f *.o
-
-
-
+	rm -f *.o essai_fap essai cesar
--- instrumentation.c
+++ instrumentation.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 #include "fap.h"
 
-// global variables
-int fap_taille_courante = 0;
-int fap_taille_max = 0;
+// remove macros so we can call the real functions
+#undef inserer
+#undef extraire
 
-// We must call the REAL functions, so declare them:
+// now declare the REAL functions
 fap inserer(fap f, int element, int priorite);
 fap extraire(fap f, int *element, int *priorite);
 
+int fap_taille_courante = 0;
+int fap_taille_max = 0;
+
 fap instrumentation_inserer(fap f, int element, int priorite) {
-    // Call the REAL insertion
-    f = inserer(f, element, priorite);
+    f = inserer(f, element, priorite);  // now calls REAL function
 
-    // Update size
     fap_taille_courante++;
-
-    // Update max
     if (fap_taille_courante > fap_taille_max) {
         fap_taille_max = fap_taille_courante;
         printf("[Instrumentation] Nouvelle taille max: %d\n", fap_taille_max);
     }
-
     return f;
 }
 
 fap instrumentation_extraire(fap f, int *element, int *priorite) {
-    // Call the real extraction
-    fap ancien = f;
-    f = extraire(f, element, priorite);
+    fap old = f;
+    f = extraire(f, element, priorite); // REAL extraction
 
-    // If extraction actually removed an element:
-    if (f != ancien) {
+    if (f != old) {
         fap_taille_courante--;
     }
-
     return f;
 }
